65a022b9c299632e6c60b62976d8333fa8136b76
S-VIN <7530023@gmail.com>
2021-02-07 00:26:53 +0300

    study

--- coursera/red/8/main.cpp
+++ coursera/red/8/main.cpp
@@ -9,60 +9,73 @@
 
 using namespace std;
 
-
 struct Deal{
     int64_t time;
     string hotelName;
-    uint32_t clientID;
+    uint64_t clientID;
     int roomCount;
 };
-
 
 class HotelManager{
 public:
     void out(){
         cout << endl << endl;
-        for(auto item : allDeals){
+
+
+        /*for(auto item : allDeals){
             cout << "--" << item.hotelName << " " << item.time << endl;
+        }*/
+
+        for(auto temp : clientsNumber) {
+            for (auto item: temp.second) {
+                cout << temp.first << ":" << item.first << "---" << item.second << endl;
+            }
         }
+
         cout << endl << endl;
     }
 
     void book(int64_t time, string hotelName, uint32_t clientID, int roomCount){
         allDeals.push_back({time, hotelName, clientID, roomCount});
 
+        clientsNumber[hotelName][clientID]++;
+
+        roomsStatistic[hotelName] += roomCount;
 
         while(allDeals[0].time + 86400 <= allDeals[allDeals.size() - 1].time){
+
+            clientsNumber[allDeals[0].hotelName][allDeals[0].clientID]--;
+            if(clientsNumber[allDeals[0].hotelName][allDeals[0].clientID] == 0){
+                clientsNumber[allDeals[0].hotelName].erase(allDeals[0].clientID);
+            }
+            roomsStatistic[allDeals[0].hotelName] -= allDeals[0].roomCount;
             allDeals.pop_front();
         }
-
+        //out();
     }
 
     uint32_t clients(string hotelName){
-
+        /*
         set<uint32_t> result;
         for(auto item : allDeals){
             if(item.hotelName == hotelName)
             result.insert(item.clientID);
         }
-
         return result.size();
+        */
+        //out();
+        return clientsNumber[hotelName].size();
     }
 
     int rooms(string hotelName){
-
-        uint64_t result = 0;
-        for(auto item : allDeals){
-            if(item.hotelName == hotelName)
-            result += item.roomCount;
-        }
-
-        return result;
+        //out();
+        return roomsStatistic[hotelName];
     }
 
 private:
     deque<Deal> allDeals;
     map<string, int> roomsStatistic;
+    map<string, map<uint64_t, int>> clientsNumber;
 };
 
 int main() {
@@ -81,7 +94,7 @@ int main() {
             if (query_type == "BOOK") {
                 int64_t time;
                 string hotelName;
-                uint32_t clientID;
+                uint64_t clientID;
                 int roomCount;
                 cin >> time >> hotelName >> clientID >> roomCount;
                 manager.book(time, hotelName, clientID, roomCount);
